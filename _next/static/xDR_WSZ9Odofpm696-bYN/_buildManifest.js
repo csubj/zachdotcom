4608ee93cc775bae89c8ca3dfefbce982ec2d94d
csubj <csubj@users.noreply.github.com>
2023-04-01 00:30:28 +0000

    deploy: 43b7885cd81092e341bab92bc379f539df1c555f

---
+++ _next/static/xDR_WSZ9Odofpm696-bYN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b006b331397fd480.css","static/chunks/pages/index-c7182dddb72a7d4c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
