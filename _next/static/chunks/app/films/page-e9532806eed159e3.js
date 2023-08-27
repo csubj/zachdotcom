9175738068e8a90ea91b6d2d429d6eface0e8789
csubj <csubj@users.noreply.github.com>
2023-08-27 03:02:55 +0000

    deploy: 79d4aed34b06e7a2378ed1d8a185a56dea54d82b

---
+++ _next/static/chunks/app/films/page-e9532806eed159e3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[850],{9946:function(e,n,s){Promise.resolve().then(s.t.bind(s,3015,23)),Promise.resolve().then(s.t.bind(s,6685,23)),Promise.resolve().then(s.t.bind(s,3222,23))}},function(e){e.O(0,[685,135,971,596,744],function(){return e(e.s=9946)}),_N_E=e.O()}]);
