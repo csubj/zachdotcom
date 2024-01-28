b354ca29478d8f70ffe8196032cb2f8dad8897e2
csubj <csubj@users.noreply.github.com>
2024-01-28 02:53:09 +0000

    deploy: 850b45e283482fd4b0b54943b1097f9d61ed4454

--- _next/static/chunks/app/page-9c2344607a78f8a1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{4437:function(n,_,e){Promise.resolve().then(e.t.bind(e,8192,23))},8192:function(n){n.exports={style:{fontFamily:"'__myFont_5650a3', '__myFont_Fallback_5650a3'"},className:"__className_5650a3"}}},function(n){n.O(0,[971,938,744],function(){return n(n.s=4437)}),_N_E=n.O()}]);
